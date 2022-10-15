e30eceed9495b2ee688eb5b5ab79c3390a0d50f6
vtotskas <vtotskas@advisable.gr>
2022-10-15 21:22:46 +0300

    zzz

--- lib/google.ts
+++ lib/google.ts
@@ -3,7 +3,7 @@ import { google } from 'googleapis'
 const googleAuth = new google.auth.GoogleAuth({
     credentials: {
         client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        private_key: process.env.GOOGLE_PRIVATE_KEY
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
     },
     scopes: ['https://www.googleapis.com/auth/youtube.readonly']
 })
