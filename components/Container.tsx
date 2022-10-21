b8d1c75bbdfc3c06789027d59f4a22ec5fe97e12
vtotskas <vtotskas@advisable.gr>
2022-10-21 11:59:02 +0300

    styling minor fixes

--- components/Container.tsx
+++ components/Container.tsx
@@ -98,7 +98,7 @@ export default function Container(props) {
 						<button
 							aria-label="Toggle Dark Mode"
 							type="button"
-							className="w-12 h-12 bg-gray-200 rounded-lg dark:bg-gray-600 flex items-center justify-center  hover:ring-2 ring-gray-300  transition-all"
+							className="w-12 h-12 border border-gray-300 text-gray-900 rounded-full flex items-center justify-center focus:ring-blue-500 focus:border-blue-500 hover:ring-2 ring-gray-300 transition-all dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 							onClick={() =>
 								setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
 							}
--- components/LanguageSwitcher.tsx
+++ components/LanguageSwitcher.tsx
@@ -52,7 +52,12 @@ const LanguageSwitcher: React.FC<{
 		[switchToLocale, onChange]
 	)
 	return (
-		<select value={value.value} onChange={languageChanged} onSelect={languageChanged}>
+		<select
+			className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-8/12 md:w-5/12 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+			value={value.value}
+			onChange={languageChanged}
+			onSelect={languageChanged}
+		>
 			{locales.map((locale) => {
 				const label = capitalize(languageNames.of(locale) ?? locale)
 				const option = {
