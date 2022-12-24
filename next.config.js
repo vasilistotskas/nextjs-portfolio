c198a0691ea63b2226aa340a51d75b3e38a612e5
vtotskas <vtotskas@advisable.gr>
2022-12-25 01:51:04 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -48,7 +48,7 @@ const ContentSecurityPolicy = `
     child-src *.youtube.com *.google.com *.twitter.com;
     worker-src 'self' blob:;
     default-src 'self' 'unsafe-inline' blob:;
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' *.google.com *.googleoptimize.com *.g.doubleclick.net *.googletagmanager.com *.google-analytics.com *.analytics.google.com *.youtube.com *.twitter.com data:;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' *.google.com *.googleoptimize.com *.g.doubleclick.net *.googletagmanager.com *.google-analytics.com *.analytics.google.com *.youtube.com *.twitter.com data: https://cdn.vercel-insights.com;
     style-src 'self' 'unsafe-inline' *.googleapis.com;
     img-src * blob: data:;
     media-src 'none';
