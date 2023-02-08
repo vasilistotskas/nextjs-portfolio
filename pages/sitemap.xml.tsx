5b4529c9eb6f851a89869f1adf896544e4fc11d6
vtotskas <vtotskas@advisable.gr>
2023-02-08 20:26:32 +0200

    sitemaps for blog posts

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,3 +1,5 @@
+import { getAllPosts } from '@lib/sanity/sanity.client'
+
 const createSitemap = (slugs) => `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         ${slugs
@@ -15,6 +17,10 @@ export async function getServerSideProps({ res }) {
 	const allPages = [
 		...['', 'about', 'dashboard', 'guestbook', 'uses', 'blog', '404', 'offline']
 	]
+
+	const posts = await getAllPosts()
+	const postPaths = posts.map((post) => `blog/${post.slug}`)
+	allPages.push(...postPaths)
 
 	res.setHeader('Content-Type', 'text/xml')
 	res.setHeader('Cache-Control', 'public, s-maxage=1200, stale-while-revalidate=600')
